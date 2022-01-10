6105f6e6324c8804f6a71e894ee89143a1fd90b4
elogeek <elodiechristin@gmail.com>
2022-01-10 12:22:29 +0100

    start project

---
+++ Model/Entity/Article.php
@@ -0,0 +1,92 @@
+<?php
+
+class Article {
+
+    private ?int $id;
+    private ?string $title;
+    private ?string $content;
+    private ?int $authorFk;
+
+    /**
+     * Article construct
+     * @param string|null $title
+     * @param string|null $content
+     * @param int|null $authorFk
+     */
+    public function __construct(int $id = null, string $title = null, string $content = null, int $authorFk=null) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->content = $content;
+        $this->authorFk = $authorFk;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    /**
+     * Return id of the article
+     * @param int|null $id
+     * @return Article
+     */
+    public function setId(?int $id): Article {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTitle(): ?string {
+        return $this->title;
+    }
+
+    /**
+     * Return the title of the article
+     * @param string|null $title
+     * @return Article
+     */
+    public function setTitle(?string $title): Article {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getContent(): ?string {
+        return $this->content;
+    }
+
+    /**
+     * Return the content of the article
+     * @param string|null $content
+     * @return Article
+     */
+    public function setContent(?string $content): Article {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getAuthorFk(): ?int {
+        return $this->authorFk;
+    }
+
+    /**
+     * Return the author of the article
+     * @param int|null $authorFk
+     * @return Article
+     */
+    public function setAuthorFk(?int $authorFk): Article {
+        $this->authorFk = $authorFk;
+        return $this;
+    }
+
+
+}
---
+++ Model/Entity/User.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Model\Entity;
+
+class User
+{
+
+    private ?int $id;
+    private ?string $name;
+    private ?string $lastname;
+    private ?string $password;
+    private ?string $mail;
+
+    /**
+     * User constructor.
+     * @param string|null $name
+     * @param string|null $lastname
+     * @param string|null $password
+     * @param int|null $id
+     * @param string|null $mail
+     */
+    public function __construct(int $id = null, string $name = null, string $lastname = null, string $password = null, string $mail = null) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->lastname = $lastname;
+        $this->password = $password;
+        $this->mail = $mail;
+    }
+
+    /**
+     * Return id of the user
+     * @return int|null
+     */
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return User
+     */
+    public function setId(?int $id): User {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Return name of the user
+     * @return string|null
+     */
+    public function getName(): ?string {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     * @return User
+     */
+    public function setName(?string $name): User {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Return lastname of the user
+     * @return string|null
+     */
+    public function getLastname(): ?string {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string|null $lastname
+     * @return User
+     */
+    public function setLastname(?string $lastname):User {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    /**
+     * Return password of the user
+     * @return string|null
+     */
+    public function getPassword(): ?string {
+        return $this->password;
+    }
+
+    /**
+     * @param string|null $password
+     * @return User
+     */
+    public function setPassword(?string $password): User {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * Return email address of the user
+     * @return string|null
+     */
+    public function getMail(): ?string {
+        return $this->mail;
+    }
+
+    /**
+     * @param string|null $mail
+     * @return User
+     */
+    public function setMail(?string $mail):User {
+        $this->mail = $mail;
+        return $this;
+    }
+
+}
