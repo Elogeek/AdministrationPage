fd12d5d6b4bad6cb3fb1b8547d71628a38058e7c
elogeek <elodiechristin@gmail.com>
2022-01-13 10:15:48 +0100

    suite

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,6 @@
 import $ from "jquery";
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-});
-
 // Or with jQuery
-
 $(document).ready(function(){
     $('.sidenav').sidenav();
 });
