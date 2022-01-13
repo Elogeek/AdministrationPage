9c5dd5f3778206e3496b26b34d3b7d5de29233f9
elogeek <elodiechristin@gmail.com>
2022-01-13 10:59:47 +0100

    suite

--- public/index.php
+++ public/index.php
@@ -20,3 +20,25 @@ $title = "Administration Page!";
 
 // Affichage du rendu d'un template
 echo $twig->render("base.html.twig", ["title" => $title]);
+
+{# Class user #}
+    class User
+    {
+        public string $name;
+        public string $lastName;
+        public string $mail;
+
+        public function __construct(string $name, string $lastN, string $mail)
+        {
+            $this->name = $name;
+            $this->lastName = $lastN;
+            $this->mail = $mail;
+        }
+    }
+
+    $doe = new User("jhon", "Doe", "doedoe@gmail.com");
+    echo $twig->render("users.html.twig", [
+        'user' => $doe,
+        'item' => ['hello', 'jhon', 'doe']
+    ]);
+}
